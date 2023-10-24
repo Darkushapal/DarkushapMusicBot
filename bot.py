b786d6930ce1f4dd591d01f98b043fef95b5f14a
Andrey Lobutev <andrey.lobutev@gmail.com>
2023-10-24 19:15:09 +0300

    0.3

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 import logging
 from config import config
-from handlers import questions, diff_types, group_games, forwarded_from_channel, downloader
+from handlers import downloader
 from middlewares.long_operation_middleware import ChatActionMiddleware
 
 logging.basicConfig(level=logging.INFO)
@@ -13,7 +13,7 @@ async def main():
     bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher()
 
-    dp.include_routers(questions.router, diff_types.router, group_games.router, forwarded_from_channel.router, downloader.router)
+    dp.include_routers(downloader.router)
     dp.message.middleware(ChatActionMiddleware())
 
     await bot.delete_webhook(drop_pending_updates=True)
--- handlers/downloader.py
+++ handlers/downloader.py
@@ -1,16 +1,41 @@
 import yt_dlp
-from aiogram import Router, F
+import os
+from aiogram import Router, F, types
 from aiogram.types import Message, CallbackQuery
-
+from aiogram.types import FSInputFile
 
 router = Router()
 
-url = "https://www.youtube.com/watch?v=RYCR55P99yg"
+video_dir = 'for_files'
 
-options = {'format': "ba"}
+options = {
+    'format': 'ba',
+    'outtmpl': f'{video_dir}/%(title)s.mp3',
+    'playlist_items': "0:1"
+}
 
 
-@router.callback_query(F.data, flags={"long_operation": "upload_video_note"})
-async def downloader(message: Message, callback: CallbackQuery):
+@router.message(F.text, flags={"long_operation": "upload_video_note"})
+async def downloader(message: Message):
+    url = message.text
+    tiny_url = url.split('&')
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        info = ydl.extract_info(url, download=False)
+
+    if len(tiny_url) > 1:
+        video_title = info['entries'][0]['title']
+    else:
+        video_title = info.get('title')
+
+    video_file = f'{video_title}.mp3'
+    video_path = f'{video_dir}\{video_file}'
+
     with yt_dlp.YoutubeDL(options) as ydl:
         ydl.download(url)
+
+    audio_final = FSInputFile(video_path)
+
+    await message.reply_audio(audio_final)
+
+    os.remove(video_path)
